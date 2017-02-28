f69fa11e998661814627bb88ffbdd6e34099afbd
Wangweiyi <wangweiyi0527@163.com>
2017-02-28 18:18:02 +0800

    cell animtation

--- tableViewSeniorDemo/tableViewSeniorDemo/delayCellAnimationVC.swift
+++ tableViewSeniorDemo/tableViewSeniorDemo/delayCellAnimationVC.swift
@@ -10,26 +10,80 @@ import UIKit
 
 class delayCellAnimationVC: UIViewController {
 
+    var tableView: UITableView?
+    var shouldStartAnimation: Bool = false
+    /*第一次push进VC的时候，列表所展示的cell是不需要动画的，而且，这些cell会在viewdidAppear之前准备好，所以动画flag会在viewdidAppear之后开启*/
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupUI()
+        self.title = "delayCellAnimation"
+    }
 
-        // Do any additional setup after loading the view.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        shouldStartAnimation = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    }
+
+
+
+
+}
+
+
+extension delayCellAnimationVC: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        if cell == nil {
+            cell = UITableViewCell.init(style: .default, reuseIdentifier: "cell")
+            cell?.contentView.backgroundColor = UIColor.cyan
+            cell?.selectionStyle = .none
+        }
+        cell?.imageView?.image = UIImage.init(named: "212")
+        cell?.textLabel?.text = "text"
+        return cell!
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+
+
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+
+        if !shouldStartAnimation {return}
+        let firstVisibleCell = tableView.visibleCells[0]
+        let firstVisibleCellIndex = tableView.indexPath(for: firstVisibleCell)
+        let factor = firstVisibleCellIndex?.row ?? 0 > indexPath.row ? -1 : 1
+        let transform = CGAffineTransform.init(translationX: 0, y: CGFloat(20 * factor))
+        cell.transform = transform
+        UIView.animate(withDuration: 1) {
+            cell.transform = CGAffineTransform.identity
+        }
+    }
+}
+
+// MARK: - UI
+extension delayCellAnimationVC {
+
+    func setupUI() {
+        self.tableView = UITableView.init(frame: CGRect.init(x: 0, y: 0, width: k_SCREEN_W, height: k_SCREEN_H))
+        self.tableView?.delegate = self
+        self.tableView?.dataSource = self
+        self.view.addSubview(self.tableView!)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
--- tableViewSeniorDemo/tableViewSeniorDemo/rightInAnimationVC.swift
+++ tableViewSeniorDemo/tableViewSeniorDemo/rightInAnimationVC.swift
@@ -10,26 +10,79 @@ import UIKit
 
 class rightInAnimationVC: UIViewController {
 
+    var tableView: UITableView?
+
+    var shouldStartAnimation: Bool = false
+    /*第一次push进VC的时候，列表所展示的cell是不需要动画的，而且，这些cell会在viewdidAppear之前准备好，所以动画flag会在viewdidAppear之后开启*/
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("viewdidload")
+        self.setupUI()
+        self.title = "rightInAnimation"
 
-        // Do any additional setup after loading the view.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        shouldStartAnimation = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    }
+
+
+
+
+}
+
+
+extension rightInAnimationVC: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        if cell == nil {
+            cell = UITableViewCell.init(style: .default, reuseIdentifier: "cell")
+            cell?.contentView.backgroundColor = UIColor.cyan
+            cell?.selectionStyle = .none
+        }
+        cell?.imageView?.image = UIImage.init(named: "212")
+        cell?.textLabel?.text = "text"
+        return cell!
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+
+
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if !shouldStartAnimation {return}
+        let transform = CGAffineTransform.init(translationX: tableView.bounds.width/2, y: 0)
+        cell.transform = transform
+        UIView.animate(withDuration: 1) {
+            cell.transform = CGAffineTransform.identity
+        }
+    }
+}
+
+// MARK: - UI
+extension rightInAnimationVC {
+
+    func setupUI() {
+        self.tableView = UITableView.init(frame: CGRect.init(x: 0, y: 0, width: k_SCREEN_W, height: k_SCREEN_H))
+        self.tableView?.delegate = self
+        self.tableView?.dataSource = self
+        self.view.addSubview(self.tableView!)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
